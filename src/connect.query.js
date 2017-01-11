d27f46bbbfcaf61f15334aefc1259e2f81ccd5a3
denisbetsi <denis@intosales.com>
2017-01-11 11:39:48 -0400

    Retry fix
    Fixes issue with uncaught exceptions which cause entire app crash due
    to ECONNRESET error on open socket

--- src/connect.query.js
+++ src/connect.query.js
@@ -8,42 +8,44 @@ var Query = {};
 /**
  * Send a query POST
  */
-Query.send = function Send(query, statements, callback) {
+Query.send = function Send(query, statements, callback, retry) {
     var node = this.node();
     var options = {
         method: 'POST',
-        path: '/_sql',
-        host: node.host || 'localhost',
-        port: node.port || 4200,
+        path:   '/_sql',
+        host:   node.host || 'localhost',
+        port:   node.port || 4200,
         //No need to specify Keep-Alive, node will use the default global agent
-    };
+    }
+
+    if(!retry) retry = 0;
 
     var request = Http.request(options);
 
     //I really hope this query is sanatized!
     var data = {
         stmt: query
-    };
+    }
 
-    if (statements.length > 0) {
+    if(statements.length > 0) {
         data.args = statements;
     }
 
-    request.write(JSON.stringify(data));
+    request.write( JSON.stringify(data) );
     request.end();
 
-    if (typeof callback === 'function') {
-        request.on('response', function (res) {
+    if(typeof callback === 'function') {
+        request.on('response', function(res) {
             var buf = '';
 
-            res.on('data', function (data) {
+            res.on('data', function(data) {
                 buf += data;
             });
 
-            res.on('end', function () {
+            res.on('end', function() {
                 var result = JSON.parse(buf);
 
-                if (result.error) {
+                if(result.error) {
                     callback(result.error, null);
                 }
                 else {
@@ -51,10 +53,20 @@ Query.send = function Send(query, statements, callback) {
                 }
             });
         });
+
+        request.on('error', function(e) {
+            if(retry<maxRetries){
+                // Try again
+                Query.send(query,statements,callback,retry+1);
+            }else{
+                callback(e, null);
+            }
+            request.abort();
+        });
     }
 
     return this;
-};
+}
 
 
 /**
